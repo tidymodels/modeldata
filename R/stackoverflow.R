fc0a2c05695b0aafe0c9228fa624e5c229906c9a
topepo <mxkuhn@gmail.com>
2019-11-28 09:52:53 -0500

    added SO data originally meant for https://github.com/tidymodels/workflows/pull/15

---
+++ R/stackoverflow.R
@@ -0,0 +1,21 @@
+#' Annual Stack Overflow Developer Survey Data
+#'
+#' @details These data are a collection of 5,594 data points collected on
+#' developers. These data could be used to try to predict who works remotely
+#' (as used in the source listed below).
+#'
+#' @name stackoverflow
+#' @aliases stackoverflow
+#' @docType data
+#' @return \item{stackoverflow}{a tibble}
+#'
+#' @source
+#' Julia Silge, _Supervised Machine Learning Case Studies in R_
+#'
+#' \url{https://supervised-ml-course.netlify.com/chapter2}
+#'
+#' Raw data: \url{https://insights.stackoverflow.com/survey/}
+#' @keywords datasets
+#' @examples
+#' data(stackoverflow)
+NULL
