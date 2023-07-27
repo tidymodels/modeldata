4f799ff84e67513ffb4e79c27bfb385b7e81c3d9
Max Kuhn <mxkuhn@gmail.com>
2023-07-27 14:35:09 -0400

    no beta symbol

--- R/steroidogenic_toxicity.R
+++ R/steroidogenic_toxicity.R
@@ -23,9 +23,9 @@
 #'  - `cortisol`: numeric
 
 #' @details
-#' H295R cells were used to measure the effect with two sets of assay reuslts.
+#' H295R cells were used to measure the effect with two sets of assay results.
 #' The first includes a set of protein measurements on: cytochrome P450 enzymes
-#' ("cyp"s), STAR, and 3$\beta$HSD2. The second include hormone measurements for
+#' ("cyp"s), STAR, and 3BHSD2. The second include hormone measurements for
 #' DHEA, progesterone, testosterone, and cortisol.
 #'
 #' Columns:
