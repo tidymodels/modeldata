4055302d4d1c8da719f729291157d7d96305a631
Max Kuhn <mxkuhn@gmail.com>
2023-12-21 12:33:23 -0500

    add dataset and re-doc (#65)
    Co-authored-by: ‘topepo’ <‘mxkuhn@gmail.com’>

---
+++ R/deliveries.R
@@ -0,0 +1,27 @@
+#' Food Delivery Time Data
+#'
+#' @details
+#' These data are from a study of food delivery times (i.e., the time from the
+#' initial order to receiving the food) for a single resturant. The data
+#' contains 10,012 orders from a specific restaurant. The predictors include:
+#' \itemize{
+#' \item The time, in decimal hours, of the order.
+#' \item The day of the week for the order.
+#' \item The approximate distance between the restaurant and the delivery
+#'       location.
+#' \item A set of 27 predictors that count the number of distinct menu items
+#'       in the order.
+#' }
+#'
+#' No times are censored.
+#'
+#' @name deliveries
+#' @aliases deliveries
+#' @docType data
+#' @return \item{deliveries}{a tibble}
+#'
+#' @keywords datasets
+#' @examples
+#' data(deliveries)
+#' str(deliveries)
+NULL
