affdf206a8205a7efb1fabf20888bdc28625c7fa
Max Kuhn <mxkuhn@gmail.com>
2026-08-06 09:01:44 -0400

    new conf data set (#86)

---
+++ R/cls_data_2026.R
@@ -0,0 +1,16 @@
+#' Two classes, Two predictors 2026 Edition
+#'
+#' Used for the 2026 posit::conf tidymdoels workshop. The data contain two
+#' predictors (`pred_1` and `pred_2`) and a factor colmnn (`class`) with
+#' levels `"class_1"` and `"class_2"`. There are 1,000 rows in the data.
+#'
+#' @name cls_data_2026
+#' @aliases cls_data_2026
+#' @docType data
+#' @return \item{cls_data_2026}{a data frame}
+#'
+#' @keywords datasets
+#' @examples
+#' data(cls_data_2026)
+#' str(cls_data_2026)
+NULL
